a16962cc07282bed0a383128d8527fa2d42d2db5
BlitzBurn <JK98@Live.se>
2021-10-10 16:22:12 +0200

    Found me files

---
+++ Lab3_C++.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Person
+{
+
+};
+
+class PersonReg 
+{
+    Person[] personArr= new Person[];
+
+    PersonReg(int maxSize) 
+    {
+
+    }
+};
