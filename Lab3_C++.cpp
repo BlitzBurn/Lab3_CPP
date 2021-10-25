fd82965ba1544040c62389977545203f0e4fb762
BlitzBurn <JK98@Live.se>
2021-10-25 22:49:06 +0200

    did some crap

--- Lab3_C++.cpp
+++ Lab3_C++.cpp
@@ -10,40 +10,83 @@ int main()
 
 class Person
 {
-    Person(const string& namn, const string& adress)
+public:
+    string namn, address;
+    /*
+    Person(string namn, string adress)
     {
-    
-    }
+        this->namn = namn;
+        this->address = namn;
+    }*/
 };
 
 class PersonReg 
 {
-    Person** personArr;
+    Person** personArr= new Person*[];
     Person* persons;
     int nrOfPersonsInArr=0;
+    int maxSize;
 
     PersonReg(int maxSize) 
     {
+        this->maxSize = maxSize;
+
+        personArr = new Person*[maxSize];
+
+       // personArr = sizeof(maxSize);
+
         for (int i = 0; i < maxSize; ++i)
         {
+
             personArr[i] = new Person[];
         }
     }
 
+    bool LäggTill(Person* personPtr) 
+    {
+        try 
+        {
+            personArr[nrOfPersonsInArr] = personPtr;
+            nrOfPersonsInArr++;
+            return true;
+        }
+        catch (Person* person) 
+        {
+            cout << "could not add person to array"<<endl;
+            return false;
+        }
+    }
 
-    bool AddPersonWithPointer() 
+    bool LäggTillTest(string namn, string adress)
+    {
+        try
+        {
+            personArr[nrOfPersonsInArr].namn = namn;
+            return true;
+        }
+        catch (Person* person)
+        {
+            return false;
+        }
+    }
+
+    void TaBortEntry(Person* ptr)
     {
 
     }
 
-    bool AddPersonWithNameAndAdress(const string& namn, const string& adress)
+    Person* SökNamn(const string& namn) const 
     {
 
     }
 
-    void TaBortEntry(Person* ptr) 
+    void Print() const
     {
 
     }
-    
+
+    void Töm()
+    {
+
+    }
 };
